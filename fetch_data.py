5c9cecee0f52dfc6812e4f3adca55656bc68e6a9
Ron Rurgundy <verlooy@protonmail.com>
2023-05-20 01:09:52 -0500

    nstr

--- fetch_data.py
+++ fetch_data.py
@@ -1,7 +1,15 @@
 import json
 # Runs every 0.02 seconds from the background thread in main.py
-def update():
+def update_conn():
     with open(".\\test.json", 'r', errors='ignore') as json_file:
+        try:
+            data = json.load(json_file, strict=False)
+        except Exception as e:
+            print(e)
+    return data
+
+def update_process():
+    with open(".\\process.json", 'r', errors='ignore') as json_file:
         try:
             data = json.load(json_file, strict=False)
         except Exception as e:
